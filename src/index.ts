295e0033e66d151a7b1707477f7d66a96c52c284
Kashif Khan <kashifkhan0336@gmail.com>
2023-10-16 09:59:40 +0500

    configured eslint

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
 import express, { Request, Response } from "express";
 
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req: Request, res: Response): Response => {
-  return res.send("Hello, World!");
-});
-
-const start = async (): Promise<void> => {
-  try {
-    app.listen(3000, () => {
-      console.log("Server started on port 3000");
-    });
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
-
-void start();
+async function startServer() {
+  const app = express();
+  app.listen(process.env.PORT, () => {
+    console.log(`Your server is ready !`);
+  });
+}
